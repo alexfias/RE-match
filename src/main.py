916250a3117c3d631aa333d3d903936c368b8a1e
Alexander Kies <kies@fias.uni-frankfurt.de>
2023-09-14 20:19:35 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
+from methods import basic_rmi
+# ... other imports and code
+
+if method_name == "basic_rmi":
+    results = basic_rmi.compute_rmi("path_to_your_network_data")
+
 if __name__ == "__main__":
     main()
